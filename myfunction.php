0dda57e2c7826301fd939d4b5c39b93ce6a2d208
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2021-10-22 15:25:40 +0200

    fixade att den lade innehållet på fel ställe genoim att bara flytta hela koden dit och sen lägga i ett nytt dokument för att det skulle bli snyggare kod

--- myfunction.php
+++ myfunction.php
@@ -18,12 +18,16 @@
                 }
                 else {
                     echo "Connection created";
+
+                    if ($interest != "") {
+                        $sqlInsertInterest = "UPDATE Users SET intresse = '$interest' WHERE username = '$username'";
+
+                        if(mysqli_query($conn, $sqlInsertInterest)){
+                            $_SESSION["interest"] = $interest;
+                        }
+                    }
                     
-                    $sqlInsertInterest = "UPDATE Users SET intresse = '$interest' WHERE username = '$username'";
-
-                    $interestQuery = mysqli_query($conn, $sqlInsertInterest);
-
-                    $_SESSION["interest"] = $interest;
+                    
                 }
 
                 if ($interestQuery) {
@@ -40,25 +44,25 @@
         }
 
         if ($_SESSION["interest"] != null) {
-            echo "du har intresse grattis".$_SESSION["interest"];
 
             $api_url = 'https://newsdata.io/api/1/news?apikey=pub_1866ec4393984dff679beb0cebe4231e94b2&language=en&category='.$_SESSION["interest"];
 
             $json_data = file_get_contents($api_url);
             try {
-                
+
+                $articleNum = rand(0, 5);
                 $response_data = json_decode($json_data,true) or exit("Your code doesn't work");
                 
-                echo "<h1>".$headerContent['results'][0]['title']."</h1>";
-                echo "<img src=".$picContent['results'][0]['image_url']."></img>";
-                echo "<p>".$textContent['results'][0]['description']."</p>";
+                echo "<h1>".$response_data['results'][$articleNum]['title']."</h1>";
+                echo "<img src=".$response_data['results'][$articleNum]['image_url']."></img>";
+                echo "<p>".$respons_data['results'][$articleNum]['description']."</p>";
 
             } catch (\Throwable $th) {
                 echo $th->getMessage();
             }
         }
         else{
-            echo "du har inte intresse :(";
+            echo "du har inget intresse :(";
         }
 
         mysqli_close($conn);
